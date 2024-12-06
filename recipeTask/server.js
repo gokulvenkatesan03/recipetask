0c50290709ac976d97aa93ea59270ebc788c64e0
gokulvenkatesan03 <gokulvenkatasan69@gmail.com>
2024-12-06 21:25:38 +0530

    Update server.js

--- recipeTask/server.js
+++ recipeTask/server.js
@@ -14,7 +14,44 @@ connectDB();
 
 // Root route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Recipe API! Navigate to /api/recipes to interact with the API.');
+    res.send('{
+             "_id": "6753175df7bb78a40ab2ce34",
+        "title": "Cookies",
+        "ingredients": [
+            "butter",
+            "sugar",
+            "eggs",
+            "vanilla extract",
+            "flour",
+            "baking soda"
+        ],
+        "instructions": "Mix butter, sugar, eggs, and vanilla essence, then add flour and baking soda. Drop spoonfuls onto a baking sheet and bake at 350°F (175°C) until golden.",
+        "prepTime": 25,
+    },
+        {
+            "_id": "67531817f7bb78a40ab2ce39",
+        "title": "Pasta",
+        "ingredients": [
+            "pasta",
+            "sauce",
+            "cheese"
+        ],
+        "instructions": "Boil pasta, add sauce, and mix.",
+        "prepTime": 30,
+        }
+        {
+     "_id": "67531940f7bb78a40ab2ce3c",
+        "title": "Cake",
+        "ingredients": [
+            "flour",
+            "sugar",
+            "eggs",
+            "butter",
+            "baking powder"
+        ],
+        "instructions": "Mix flour, sugar, eggs, butter, and baking powder. Add milk and vanilla extract until smooth.",
+        "prepTime": 45,
+        }');
 });
 
 // Routes
